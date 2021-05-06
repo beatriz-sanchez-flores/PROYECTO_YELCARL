2eb74f931e58dda5dce977c4ecd7a709544cd62f
Torrez Rolan <FerlyGuti-0002>
2021-05-06 10:54:06 -0600

    Cree conexión del sistema con BD

---
+++ YELCARL/CONEXION.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace YELCARL
+{
+    class CONEXION
+    {
+
+        //C<adena de conexióN
+        string cadena = "Data source=LAPTOP-JQJNVT0F\\SQLEXPRESS01;Initial Catalog=YELCARL; Integrated Security=True";
+        //Crear conexión public
+        public SqlConnection conectarbd = new SqlConnection();
+        //Agregar constructor
+        public CONEXION()
+        {
+            conectarbd.ConnectionString = cadena; //Objeto creado
+        }
+
+        //Crear metodo para abrir y cerrar conexión
+        public void abrir()
+        {   //Sección para capturar errores
+            try
+            {   //Intentar abrir conexión
+                conectarbd.Open();
+                Console.WriteLine("Conexión abierta ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al abrir BD " + ex.Message);
+            }
+        }
+        //Procedimiento a cerrar
+        public void cerrar()
+        {
+            conectarbd.Close();
+        }
+    }
+}
